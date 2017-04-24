2b5af7074c3021f24e593e9291c90e773860f730
ktykty <619760533@qq.com>
2017-04-24 23:35:42 +0800

    'junit'

---
+++ src/test/java/jdbc/jdbcUntilTest.java
@@ -0,0 +1,45 @@
+package test.jdbc; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* jdbcUntil Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ËÄÔÂ 24, 2017</pre> 
+* @version 1.0 
+*/ 
+public class jdbcUntilTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getConnection() 
+* 
+*/ 
+@Test
+public void testGetConnection() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: free(ResultSet rs, Statement st, Connection conn) 
+* 
+*/ 
+@Test
+public void testFree() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
